6167cd9f3f741a123649b7cf2fa8d9f010b0a243
Henrich Horváth <dev@henrich-horvath.eu>
2017-11-23 23:41:00 +0100

    fixed test

--- BL.Tests/FacadeTests/RegisteredUserFacadeTest.cs
+++ BL.Tests/FacadeTests/RegisteredUserFacadeTest.cs
@@ -2,6 +2,7 @@
 using AutoMapper;
 using BL.Tests.FacadeTests.Common;
 using BusinessLayer.DTOs;
+using BusinessLayer.DTOs.Common;
 using BusinessLayer.DTOs.Filters;
 using BusinessLayer.Facades.Employers;
 using BusinessLayer.QueryObjects.Common;
@@ -41,6 +42,40 @@ namespace BL.Tests.FacadeTests
             EmployerFacade userfacade = CreateEmployerFacade(employerRepositoryMock, employerQueryMock);
 
             EmployerDto foundEmployer = await userfacade.Get(id);
+
+            Assert.AreEqual(foundEmployer, expectedEmployerDto);
+        }
+
+        [Test]
+        public async Task Test_GetByName()
+        {
+            const int id = 1337;
+            const string name = "Amazon";
+            EmployerDto expectedEmployerDto = new EmployerDto
+            {
+                Id = id,
+                Name = name,
+                Email = "mail@amazon.com"
+            };
+            Employer expectedEmployer = new Employer
+            {
+                Id = id,
+                Name = name,
+                Email = "mail@amazon.com"
+            };
+            var queryResult = new QueryResultDto<EmployerDto, EmployerFilterDto>()
+            {
+                Filter = new EmployerFilterDto {Name = name},
+                Items = new[] {expectedEmployerDto},
+                TotalItemsCount = 1
+            };
+
+            FacadeMockManager mockManager = new FacadeMockManager();
+            var employerRepositoryMock = mockManager.ConfigureGetRepositoryMock(expectedEmployer);
+            var employerQueryMock = mockManager.ConfigureQueryObjectMock<EmployerDto, Employer, EmployerFilterDto>(queryResult);
+            EmployerFacade userfacade = CreateEmployerFacade(employerRepositoryMock, employerQueryMock);
+
+            EmployerDto foundEmployer = await userfacade.GetByName(name);
 
             Assert.AreEqual(foundEmployer, expectedEmployerDto);
         }
